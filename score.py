05c506e486dcb02123272157473a5e4a2e1af66d
zacharythomasabraham <zachary.thomas.abraham@gmail.com>
2016-06-25 12:11:55 -0700

    score for warriors Vs cavs

---
+++ score.py
@@ -0,0 +1,9 @@
+#Create 2 new variables warriors and cavs
+#Check whether team1 > teams Congrats Warriors
+#Come on Warriors
+warriors = 76
+cavs = 56
+if(warriors > cavs):
+    print("Congrats Warriors!")
+else:
+    print("Come on Warriors!")
